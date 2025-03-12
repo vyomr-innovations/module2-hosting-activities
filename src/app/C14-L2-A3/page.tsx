fccd8aa38f5a566610b41fbaba095fd3dfb3c0c2
mohammad asad <asadkhan41376@gmail.com>
2025-03-12 08:39:51 +0530

    image change in C14-L2-A3

--- src/app/C14-L2-A3/page.tsx
+++ src/app/C14-L2-A3/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
         <h4 className='text-black text-3xl py-8'>Six Word Story</h4>
         <div className='shadow-black shadow'>
             
-            <Image src="/image/C14-L2-A3.jpg" width={900} height={200} alt='image'/>
+            <Image src="/image/C14-L2-A3-change.jpg" width={900} height={200} alt='image'/>
         </div>
       
     </div>
